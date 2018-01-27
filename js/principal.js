334a59a39a85b82c3735a0ef2a2f077af2633b99
Fabio Moreira <fabiomsnet@gmail.com>
2018-01-27 17:02:11 -0200

    Exercício 5.11

--- js/principal.js
+++ js/principal.js
@@ -106,3 +106,17 @@ function decideTipoCartao(conteudo) {
 
     return tipoCartao;
 }
+
+$("#busca").on("input", function() {
+    var busca = $(this).val().trim();
+
+    if (busca.length) {
+        $(".cartao").hide().filter(function() {
+            return $(this).find(".cartao-conteudo")
+                            .text()
+                            .match(new RegExp(busca, "i"));
+        }).show();
+    } else {
+        $(".cartao").show();
+    }
+});
