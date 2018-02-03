daf1e1cbc76c741943a56b6db27fcc05c206ea37
Fabio Moreira <fabiomsnet@gmail.com>
2018-02-03 15:27:09 -0200

    Iniciando com módulos

--- js/adicionaCartao.js
+++ js/adicionaCartao.js
@@ -1,26 +1,73 @@
-var contador = $(".cartao").length;
+var adicionaCartao = (function () {
 
-function adicionaCartao(conteudo, cor) {
-    contador++;
+    function removeCartao() {
+        var cartao = document.querySelector("#cartao_" + this.dataset.ref);
 
-    var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
-        .attr("data-ref", contador)
-        .text("Remover")
-        .click(removeCartao);
+        cartao.classList.add("cartao--some");
+        setTimeout(function () {
+            cartao.remove();
+        }, 400);
+    }
 
-    var opcoes = $("<div>").addClass("opcoesDoCartao")
-        .append(botaoRemove);
+    var botoes = document.querySelectorAll(".opcoesDoCartao-remove");
 
-    var tipoCartao = decideTipoCartao(conteudo);
+    for (var i = 0; i < botoes.length; i++) {
+        botoes[i].addEventListener("click", removeCartao);
+    }
 
-    var conteudoTag = $("<p>").addClass("cartao-conteudo")
-        .append(conteudo);
+    function decideTipoCartao(conteudo) {
+        var quebras = conteudo.split("<br>").length;
 
-    $("<div>").attr("id", "cartao_" + contador)
-        .addClass("cartao")
-        .addClass(tipoCartao)
-        .append(opcoes)
-        .append(conteudoTag)
-        .css("background-color", cor)
-        .prependTo(".mural");
-}
+        var totalDeLetras = conteudo.replace(/<br>/g, " ").length;
+
+        var ultimoMaior = "";
+        conteudo.replace(/<br>/g, " ")
+            .split(" ")
+            .forEach(function (palavra) {
+                if (palavra.length > ultimoMaior.length) {
+                    ultimoMaior = palavra;
+                }
+            });
+
+        var tamMaior = ultimoMaior.length;
+
+        //no mínimo, todo cartão tem o texto pequeno
+        var tipoCartao = "cartao--textoPequeno";
+
+        if (tamMaior < 9 && quebras < 5 && totalDeLetras < 55) {
+            tipoCartao = "cartao--textoGrande";
+        } else if (tamMaior < 12 && quebras < 6 && totalDeLetras < 75) {
+            tipoCartao = "cartao--textoMedio";
+        }
+
+        return tipoCartao;
+    }
+
+
+    var contador = 0;
+
+    return function (conteudo, cor) {
+        contador++;
+
+        var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
+            .attr("data-ref", contador)
+            .text("Remover")
+            .click(removeCartao);
+
+        var opcoes = $("<div>").addClass("opcoesDoCartao")
+            .append(botaoRemove);
+
+        var tipoCartao = decideTipoCartao(conteudo);
+
+        var conteudoTag = $("<p>").addClass("cartao-conteudo")
+            .append(conteudo);
+
+        $("<div>").attr("id", "cartao_" + contador)
+            .addClass("cartao")
+            .addClass(tipoCartao)
+            .append(opcoes)
+            .append(conteudoTag)
+            .css("background-color", cor)
+            .prependTo(".mural");
+    }
+})();
